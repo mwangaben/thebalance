f7bc3c0e3eb7cbaa39c2206319abd21ebd17a2ed
Benedict Mwanga <benedict@Benedicts-MacBook-Air.local>
2017-06-15 05:11:10 +0300

    Show the corrent Balance up on inflow and ouflow in the records

--- app/Benny/Mathematics/Solve.php
+++ app/Benny/Mathematics/Solve.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 
 namespace App\Benny\Mathematics;
 
+use App\Movement;
 
-class Solve {
-	
-	public function updateBalance(array $data, $balance)
-	{
-		  if ($data['type'] == 'input') {
-		  	    foreach ($data as $input) {
-		  	    	$balance += $input;
-		  	    }
-		  }else {
-		  	
-		  }
-	}
+class Solve
+{
+
+
+    public function newBalance($denomination, $balance)
+    {
+        $inputs  = Movement::all()->where('type', 'input')->sum($denomination);
+        $outputs = Movement::all()->where('type', 'output')->sum($denomination);
+        return $balance += $inputs - $outputs;
+
+    }
 }
- ?>
--- tests/Feature/MovementTest.php
+++ tests/Feature/MovementTest.php
@@ -2,26 +2,93 @@
 
 namespace Tests\Feature;
 
+use App\Benny\Mathematics\Solve;
 use App\Movement;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
 class MovementTest extends TestCase
 {
 
-	use DatabaseTransactions, DatabaseMigrations;
+    use DatabaseTransactions, DatabaseMigrations;
+
+    /** @test **/
+    public function it_gets_new_balance_from_inflow()
+    {
+        factory(Movement::class)->create([
+            'tenDeno'  => 1000,
+            'fiveDeno' => 500,
+            'type'     => 'input',
+        ]);
+
+        $this->assertEquals(3000, (new Solve())->newBalance('tenDeno', 2000));
+    }
+
+    /** @test **/
+    public function it_gets_new_balance_from_outflow()
+    {
+        factory(Movement::class)->create([
+            'tenDeno'  => 1000,
+            'fiveDeno' => 500,
+            'type'     => 'output',
+        ]);
+
+        $this->assertEquals(4000, (new Solve())->newBalance('tenDeno', 5000));
+    }
+
+    /** @test **/
+    public function it_gets_new_balance_from_multiple_inflow()
+    {
+        factory(Movement::class)->create([
+            'tenDeno'  => 4000,
+            'fiveDeno' => 500,
+            'type'     => 'input',
+        ]);
+        factory(Movement::class)->create([
+            'tenDeno'  => 3000,
+            'fiveDeno' => 500,
+            'type'     => 'input',
+        ]);
+
+        $this->assertEquals(17000, (new Solve())->newBalance('tenDeno', 10000));
+
+    }
+
       /** @test **/ 
-	function it_check_if_the_user_can_pull_data()
+	function it_gets_new_balance_from_multiple_outflow()
 	{
-		$move = factory(Movement::class)->create();
-		// $this->assertEquals(expected, actual);
-		$this->assertDatabaseHas('movements', [
-				'tenDeno' => $move->tenDeno,
-				'fiveDeno' =>$move->fiveDeno,
-				'type' => $move->type,
+		factory(Movement::class)->create([
+             'tenDeno' => 1000,
+             'fiveDeno' => 500,
+             'type' => 'output',
 			]);
+
+		factory(Movement::class)->create([
+             'tenDeno' => 2000,
+             'fiveDeno' => 1000,
+             'type' => 'output',
+			]);
+
+		$this->assertEquals(10000, (new Solve())->newBalance('tenDeno', 13000));
 		
+	}
+
+	  /** @test **/ 
+	function it_get_new_balance_from_both_inflow_and_outflow()
+	{
+          factory(MOvement::class)->create([
+               'tenDeno' => 1000,
+               'fiveDeno'=> 500,
+               'type' => 'input'
+          	]);
+
+         factory(MOvement::class)->create([
+              'tenDeno' => 2000,
+              'fiveDeno' => 1000,
+              'type' => 'output',
+         ]);
+
+         $this->assertEquals(4000, (new Solve())->newBalance('tenDeno', 5000)); 			
 	}
 }
